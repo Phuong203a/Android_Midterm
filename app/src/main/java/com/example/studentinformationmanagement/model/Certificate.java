f6d152683f2fa389809716bab1c73eb5b42d2faf
Administrator <hung.nt20@gmail.com>
2023-11-19 14:27:36 +0700

    add layout

---
+++ app/src/main/java/com/example/studentinformationmanagement/model/Certificate.java
@@ -0,0 +1,13 @@
+package com.example.studentinformationmanagement.model;
+
+public class Certificate {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
